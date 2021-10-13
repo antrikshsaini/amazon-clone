8f66ff13847304d02e429ac460585a4a47e4b7ad
Antrix <sainiantriksh6@gmail.com>
2021-10-12 22:54:31 -0700

    First

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        // Logo //
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+      <div className="header__search">
+        {/* Search Bar */}
+        <input className="header__searchInput" type="text" />
+      </div>
+
+      <div className="header__nav">
+        {/* Options */}
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAs6X2cGbpB7PV8jdhbReEorsuBJTehxCY',
+  authDomain: 'clone-8a5ba.firebaseapp.com',
+  projectId: 'clone-8a5ba',
+  storageBucket: 'clone-8a5ba.appspot.com',
+  messagingSenderId: '765561947188',
+  appId: '1:765561947188:web:c3076379612ebaff1c7661',
+  measurementId: 'G-VEJNCQ5H23',
+};
