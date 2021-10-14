61612bdcbb96e232c9eddadcc294d0ade6e7b851
Antrix <sainiantriksh6@gmail.com>
2021-10-13 22:37:37 -0700

    Header update

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
@@ -13,6 +15,7 @@ function Header() {
       <div className="header__search">
         {/* Search Bar */}
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -28,6 +31,11 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
