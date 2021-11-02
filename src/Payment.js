93209fdcc24dca1c6486752835f18b559ea509f1
Antrix <sainiantriksh6@gmail.com>
2021-11-01 22:04:52 -0700

    implementing Payment page

---
+++ src/Payment.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import CheckoutProduct from './CheckoutProduct';
+import './Payment.css';
+import { useStateValue } from './StateProvider';
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React sanso</p>
+            <p>Vancouver, BC</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
